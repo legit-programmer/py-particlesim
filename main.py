87c9b4178e888f46368ceaeb7a8db5a3d7ef5683
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-09-23 20:06:20 +0530

    particle playground

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+from particle import *
+import random
 
 # Initialize Pygame
 pygame.init()
@@ -12,6 +14,7 @@ FPS = 60
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
 
 # Create the game window
 win = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -20,10 +23,23 @@ pygame.display.set_caption("Pygame Starter Template")
 # Clock for controlling the frame rate
 clock = pygame.time.Clock()
 
+
+particles = []
+# init particles
+def generateParticles(count:int):
+    for i in range(count):
+        particles.append(Particle(random.randrange(0, WIDTH, 5), random.randrange(0, HEIGHT, 5), random.randrange(10, 50), 50, 1, 1, (random.randrange(0, 255, 10), random.randrange(0, 255, 10), random.randrange(0, 255, 10))))
+
+# x, y, r, m, xvel, yvel
+
+
+generateParticles(1000)
 # Game loop
 running = True
 while running:
+
     # Event handling
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -35,6 +51,11 @@ while running:
 
     # Draw your game elements here
 
+    for particle in particles:
+        particle.draw(win)
+        particle.applyMotion()
+        particle.checkCollision(win, particles)
+        # particle.applyForce(particles)
     # Update the screen
     pygame.display.flip()
 
--- particle.py
+++ particle.py
@@ -1,11 +1,67 @@
 import pygame
+import math
+
 
 class Particle:
-    def __init__(self, x, y, r, mass, x_vel, y_vel) -> None:
+    def __init__(self, x, y, r, mass, x_vel, y_vel, color) -> None:
         self.x = x
         self.y = y
         self.r = r
         self.mass = mass
-        self.x_vel
-    
+        self.x_vel = x_vel
+        self.y_vel = y_vel
+        self.color = color
+        self.force_x = 1
+        self.force_y = 1
 
+    def draw(self, win: pygame.Surface):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.r)
+        # pygame.draw.circle(win, self.color, (250, 25), 25)
+
+    def applyMotion(self):
+        # self.x_vel *= self.force_x
+        # self.y_vel *= self.force_y
+        self.x += self.x_vel 
+        self.y += self.y_vel
+        
+
+    def checkCollision(self, win: pygame.Surface, particles:list):
+        if self.x-self.r <= 0:
+            self.x_vel = -(self.x_vel)
+
+        if self.x+self.r >= win.get_width():
+            self.x_vel = -(self.x_vel)
+
+        if self.y-self.r <= 0:
+            self.y_vel = -(self.y_vel)
+
+        if self.y+self.r >= win.get_height():
+            self.y_vel = -(self.y_vel)
+
+
+        for particle in particles:
+            if self!=particle:
+                distance = math.sqrt((particle.x-self.x) **
+                                    2 + (particle.y-self.y)**2)
+                if distance<(particle.r + self.r):
+                    particle.x_vel = -(particle.x_vel)
+                    self.x_vel = -(self.x_vel)
+                    particle.y_vel = -(particle.y_vel)
+                    self.y_vel = -(self.y_vel)
+
+    def applyForce(self, particles: list):
+        for particle in particles:
+            if particle != self:
+
+                distance = math.sqrt((particle.x-self.x) **
+                                    2 + (particle.y-self.y)**2)
+                force = (self.mass*particle.mass)/distance
+                sintheta = distance / (particle.y - self.y)
+                # print(sintheta)
+                theta_rad = math.asin(0.5)
+                print(theta_rad)
+                
+                self.force_x = math.cos(theta_rad)*force
+                print(self)
+                self.force_y = math.sin(theta_rad)*force
+                
