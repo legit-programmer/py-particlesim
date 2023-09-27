8235120bd0bdbf98b1e152e9f326d6d7662425b8
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-09-27 22:19:39 +0530

    implemented interactions between particles

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import pygame
 import sys
 from particle import *
 import random
+import itertools
+import math
 
 # Initialize Pygame
 pygame.init()
 
 # Constants
-WIDTH = 800
-HEIGHT = 800
+WIDTH = 1280
+HEIGHT = 720
 FPS = 60
 
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
 # Create the game window
 win = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -24,16 +28,60 @@ pygame.display.set_caption("Pygame Starter Template")
 clock = pygame.time.Clock()
 
 
-particles = []
+blue_particles = []
+red_particles = []
+yellow_particles = []
 # init particles
-def generateParticles(count:int):
-    for i in range(count):
-        particles.append(Particle(random.randrange(0, WIDTH, 5), random.randrange(0, HEIGHT, 5), random.randrange(10, 50), 50, 1, 1, (random.randrange(0, 255, 10), random.randrange(0, 255, 10), random.randrange(0, 255, 10))))
 
+
+def generateParticles(bcount, rcount, ycount):
+    for i in range(bcount):
+        blue_particles.append(Particle(random.randrange(
+            WIDTH), random.randrange(HEIGHT), 2, 1, 1, BLUE))
+
+    for i in range(rcount):
+        red_particles.append(Particle(random.randrange(
+            WIDTH), random.randrange(HEIGHT), 2, 1, 1, RED))
+
+    for i in range(ycount):
+        yellow_particles.append(Particle(random.randrange(
+            WIDTH), random.randrange(HEIGHT), 2, 1, 1, GREEN))
 # x, y, r, m, xvel, yvel
 
 
-generateParticles(1000)
+def applyRule(particle1: list, particle2: list, strength: int):
+    for i in particle1:
+        for j in particle2:
+            distance = math.sqrt((j.x-i.x)**2 + (j.y-i.y)**2)
+            if distance < 250:
+                force = strength
+                ux = random.choice([1, -1])
+                uy = random.choice([1, -1])
+                try:
+                    ux = (j.x-i.x) / distance
+                    uy = (j.y-i.y) / distance
+                except ZeroDivisionError:
+                    pass
+                print(ux, uy)
+                if i.x >= WIDTH:
+                    i.x = WIDTH - 5
+                    ux = -(ux)
+                if i.y >= HEIGHT:
+                    i.y = HEIGHT - 5
+                    ux = -(ux)
+                if i.y <= 0:
+                    i.y = 0 + 5
+                    ux = -(ux)
+                if i.x <= 0:
+                    i.x = 0 + 5
+                    ux = -(ux)
+                i.x += ux * force
+                i.y += uy * force
+    # print(force)
+
+
+
+generateParticles(13, 12, 45)
 # Game loop
 running = True
 while running:
@@ -51,11 +99,16 @@ while running:
 
     # Draw your game elements here
 
-    for particle in particles:
+    for particle in itertools.chain(blue_particles, red_particles, yellow_particles):
         particle.draw(win)
-        particle.applyMotion()
-        particle.checkCollision(win, particles)
-        # particle.applyForce(particles)
+    applyRule(blue_particles, red_particles, 1)
+    applyRule(red_particles, blue_particles, -5)
+    applyRule(red_particles, red_particles, 10)
+    applyRule(blue_particles, blue_particles, -1)
+    applyRule(blue_particles, yellow_particles, 5)
+    applyRule(red_particles, yellow_particles, -3)
+    applyRule(yellow_particles, red_particles, 2)
+
     # Update the screen
     pygame.display.flip()
 
